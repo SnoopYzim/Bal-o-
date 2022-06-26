1347333925af99c7bc89e47897fc1f83685440e9
SnoopY_BR <106285227+SnoopYzim@users.noreply.github.com>
2022-06-26 11:33:11 -0300

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,127 @@
+let bow , arrow,  background, verde, rosa, azul;
+let bowImage, arrowImage, green_balloonImage, red_balloonImage, pink_balloonImage ,blue_balloonImage, backgroundImage;
+
+function preload(){
+  
+  backgroundImage = loadImage("background0.png");
+  arrowImage = loadImage("arrow0.png");
+  bowImage = loadImage("bow0.png");
+  red_balloonImage = loadImage("red_balloon0.png");
+  //carregar Imagem dos 3 balões
+  verde = loadImage("grenn_balloonImage");
+  rosa = loadImage("pink_balloonImage");
+  azul = loadImage("blue_balloonImage");
+  
+}
+
+
+
+function setup() {
+  createCanvas(400, 400);
+  
+  //criar um plano de fundo
+  scene = createSprite(0,0,400,400);
+  scene.addImage(backgroundImage);
+  scene.scale = 2.5
+  
+  // criar um arco para a flecha
+  bow = createSprite(380,220,20,50);
+  bow.addImage(bowImage); 
+  bow.scale = 1;
+  
+}
+
+function draw() {
+ background(0);
+    // mover o chão
+    scene.velocityX = -3 
+
+    if (scene.x < 0){
+      scene.x = scene.width/2;
+    }
+  
+    //mover o arco
+    bow.y = World.mouseY
+  
+    // soltar a flecha quando a tecla espaço for pressionada
+     if (keyDown("space")) {
+     createArrow();
+    
+  }
+  
+    //mude o valor do balão aleatório para 4
+    let azul = Math.round(random(1,4));
+  
+  if (World.frameCount % 100 == 0) {
+   switch(select_balloon){
+    case 1: //chamar o balãovermelho
+    let balãovermelho = Math.round(random(1,4));
+    break;
+    case 2://chamar o balãoazul
+    let balãoazul = Math.round(random(1,4));
+    break;
+    case 3://chamar o balãoverde
+    let balãoverde = Math.round(random(1,4));
+    case 4://chamar o balãorosa)
+    let balãorosa = Math.round(random(1,4));
+    break;
+
+  }}
+  
+  drawSprites();
+}
+
+
+// criar flechas para o arco
+ function createArrow() {
+  let arrow= createSprite(100, 100, 60, 10);
+  arrow.addImage(arrowImage);
+  arrow.x = 360;
+  arrow.y=bow.y;
+  arrow.velocityX = -4;
+  arrow.lifetime = 100;
+  arrow.scale = 0.3;
+}
+
+
+function redBalloon() {
+  let red = createSprite(0,Math.round(random(20, 370)), 10, 10);
+  red.addImage(red_balloonImage);
+  red.velocityX = 3;
+  red.lifetime = 150;
+  red.scale = 0.1;
+
+}
+
+function blueBalloon() {
+  //crieSprite para o balão 
+  let blueBalloon = createSprite(200,200,15,15);
+  //adicioneImagem para o balão
+  blueBalloon.addImage(azul);
+  //adicione velocidade para fazer o balão se mover
+  blueBalloon.velocityY = 1.0;
+  //mudar a dimensão do balão
+  //atribua tempo de vida ao balão
+}
+
+function greenBalloon() {
+  //crieSprite para o balão
+  let greenBalloon = createSprite(150,150,15,15);
+  //adicioneImagem para o balão
+  greenBalloon.addImage(verde);
+  //adicione velocidade para fazer o balão se mover
+  greenBalloon.velocityY = 1.5;
+  //mudar a dimensão do balão
+  //atribua tempo de vida ao balão
+}
+
+function pinkBalloon() {
+  //crieSprite para o balão
+  pinkBalloon = createSprite(120,120,15,15);
+  //adicioneImagem para o balão
+  pinkBalloon.addImage(rosa);
+  //adicione velocidade para fazer o balão se mover
+  pinkBalloon.velocityY = 0.9
+  //mudar a dimensão do balão
+  //atribua tempo de vida ao balão
+}
